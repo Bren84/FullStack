e6d77211c5cbfb2d2a2b154eed5647e955f9cef7
Bren84 <42686338+Bren84@users.noreply.github.com>
2021-09-06 20:24:52 -0300

    pruebaMP

---
+++ Funciones EXPRESADAS y declaradas.js
@@ -0,0 +1,15 @@
+// FUNCION EXPRESADA
+
+let sumar = function (a, b){
+    return a+b;
+}
+
+console.log(sumar(2,3));
+
+// FUNCION declarada
+
+function restar(numA, numB) {
+    return numA-numB;
+}
+
+console.log(restar(10,3));
